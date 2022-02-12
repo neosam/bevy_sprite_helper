1402147583ab8363073833e51541875ca703908f
Simon Goller <neosam+github@posteo.de>
2022-02-12 10:22:47 +0100

    Simplified sprite loading

---
+++ examples/sprite_loader.rs
@@ -0,0 +1,67 @@
+use bevy::prelude::*;
+use bevy_ingame_tools::sprites;
+
+
+#[derive(Reflect, Default)]
+pub struct Sprites {
+    tree: usize,
+    player_south_base: usize,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug, Hash)]
+pub enum GameState {
+    Loading,
+    Ingame,
+}
+
+pub fn setup(
+    mut commands: Commands,
+    texture_atlas_holder: Res<sprites::TextureAtlasHolder>,
+    sprites: Res<Sprites>,
+) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    commands.spawn_bundle(SpriteSheetBundle {
+        texture_atlas: texture_atlas_holder.texture_atlas.clone(),
+        sprite: TextureAtlasSprite {
+            index: sprites.tree,
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+    commands.spawn_bundle(SpriteSheetBundle {
+        texture_atlas: texture_atlas_holder.texture_atlas.clone(),
+        sprite: TextureAtlasSprite {
+            index: sprites.player_south_base,
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(50.0, 0.0, 100.0),
+        ..Default::default()
+    });
+}
+
+pub fn wait_loading(
+    status: Res<sprites::SpriteGraphicsStatus>,
+    mut state: ResMut<State<GameState>>,
+) {
+    if status.is_loading_finished() {
+        state.replace(GameState::Ingame).unwrap();
+    }
+}
+
+
+
+pub fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_state(GameState::Loading)
+        .insert_resource(Sprites::default())
+        .add_system_set(SystemSet::on_enter(GameState::Loading)
+            .with_system(sprites::loading_startup::<Sprites>))
+        .add_system_set(SystemSet::on_update(GameState::Loading)
+            .with_system(sprites::loading_update::<Sprites>)
+            .with_system(wait_loading))
+        .add_system_set(SystemSet::on_enter(GameState::Ingame)
+            .with_system(setup))
+        .run();
+}
---
+++ src/sprites.rs
@@ -0,0 +1,84 @@
+use bevy::{prelude::*, reflect::{ReflectRef, ReflectMut}, asset::LoadState};
+
+pub struct SpriteGraphicsHandles {
+    sprites: Vec<(String, Handle<Image>)>,
+}
+pub struct SpriteGraphicsStatus {
+    done: bool,
+}
+impl SpriteGraphicsStatus {
+    pub fn is_loading_finished(&self) -> bool {
+        self.done
+    }
+}
+
+pub struct TextureAtlasHolder {
+    pub texture_atlas: Handle<TextureAtlas>,
+}
+
+pub fn loading_startup<T: Reflect>(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    handle_resource: Res<T>,
+) {
+    let handles = SpriteGraphicsHandles {
+        sprites: if let ReflectRef::Struct(reflect_struct) = handle_resource.reflect_ref() {
+            //let mut result = Vec::with_capacity(reflect_struct.field_len());
+            //for i in 0..reflect_struct.field_len() {
+            //    if reflect_struct.field_at(i).unwrap().type_name() == 0usize.type_name() {
+            //        let field_name = reflect_struct.name_at(i).unwrap().to_string();
+            //        let handle = asset_server.load(&format!("sprites/{field_name}.png"));
+            //        result.push((field_name, handle));
+            //    }
+            //}
+            //result
+            (0..).zip(reflect_struct.iter_fields())
+                .filter(|(_, reflect)| reflect.type_name() == 0usize.type_name())
+                .map(|(i, _)| reflect_struct.name_at(i).expect("name_at should not be out of range").to_string())
+                .map(|field_name| (asset_server.load(&format!("sprites/{field_name}.png")), field_name))
+                .map(|(handle, field_name)| (field_name, handle))
+                .collect()
+        } else {
+            Vec::new()
+        }
+    };
+    commands.insert_resource(handles);
+    commands.insert_resource(SpriteGraphicsStatus { done: false });
+}
+
+pub fn loading_update<T: Reflect>(
+    mut commands: Commands,
+    mut handles: ResMut<SpriteGraphicsHandles>,
+    asset_server: Res<AssetServer>,
+    mut textures: ResMut<Assets<Image>>,
+    mut handle_resource: ResMut<T>,
+    mut texture_atlasses: ResMut<Assets<TextureAtlas>>,
+    mut status: ResMut<SpriteGraphicsStatus>,
+) {
+    if asset_server.get_group_load_state(handles.sprites.iter().map(|(_, handle)| handle.id))
+            == LoadState::Loaded {
+        let mut texture_atlas_builder = TextureAtlasBuilder::default();                
+        for handle in handles.sprites.iter().map(|(_, handle)| handle) {
+            let texture = textures.get(handle).expect("Texture asset not found");
+            texture_atlas_builder.add_texture(handle.clone_weak(), texture);
+        }
+        let texture_atlas = texture_atlas_builder
+            .finish(&mut textures)
+            .expect("Could not create texture atlas");
+
+        if let ReflectMut::Struct(reflect_struct) = handle_resource.reflect_mut() {
+            for i in 0..reflect_struct.field_len() {
+                let field_name = reflect_struct.name_at(i).expect("name_at should not be out of range").to_string();
+                let field = reflect_struct.field_at_mut(i).expect("field_at_mut should not be out of range");
+                let sprite_filename = format!("sprites/{field_name}.png");
+                let texture_index = texture_atlas
+                    .get_texture_index(&asset_server.get_handle(sprite_filename.clone())).expect(&format!("sprite not loaded: {sprite_filename}"));
+                field.apply(&texture_index);
+            }
+        }
+        let texture_atlas = texture_atlasses.add(texture_atlas);
+        commands.insert_resource(TextureAtlasHolder { texture_atlas });
+        handles.sprites.clear();
+        status.done = true;
+    }
+}
